83a17c8db28f9e8fc0f90ebd3852cfa121802cc7
Xavier0217 <arthur.xavier016002@gmail.com>
2023-08-17 10:18:12 -0300

    Aprendendo enumerações em java :)

---
+++ Fundamentos/src/enumeracao/Enumeracao.java
@@ -0,0 +1,17 @@
+package enumeracao;
+
+
+public class Enumeracao {
+    public static void main(String[] args) {
+        
+        Usuario usuario = new Usuario("Usuario01", "usuario01@gmail.com", true, TipoUsuarioEnum.TECNICO);
+        System.out.println(usuario);
+        
+        Pedido pedido = new Pedido("Pedido Usuario01", 29.99, StatusEnum.EM_PREPARACAO.getStatus());
+        System.out.println(pedido);
+        pedido.setStatus(StatusEnum.EM_TRANSPORTE.getStatus());
+        System.out.println(pedido);
+        pedido.setStatus(StatusEnum.ENTREGUE.getStatus());
+        System.out.println(pedido);
+    }
+}
